809aeb18ec2afa2718454b51d92c36db4faadc7d
Khaustov T.F. <trofimhaustov@yandex.ru>
2025-04-13 20:20:06 +0800

    dz 41

--- src/main/kotlin/HM4.1.kt
+++ src/main/kotlin/HM4.1.kt
@@ -18,12 +18,18 @@ object WallService {
     private var unickId = 0
 
     fun add(post: Post): Post {
-        posts += post.copy(++unickId)
+        posts += post.copy(++unickId, likes = post.likes.copy())
         return posts.last()
+
     }
 
     fun update(post: Post): Boolean {
-
+        for ((index,post) in posts.withIndex()) {
+            if (post.id == post.id){
+                posts[index] = post.copy(likes = post.likes.copy())
+                return true
+            }
+        }
         return false
     }
 
@@ -32,12 +38,12 @@ object WallService {
             print(post)
             println()
         }
-        println()
     }
 }
 
 fun main() {
     WallService.add(Post(1,2,3,"Post", 4, false, false))
     WallService.add(Post(1,3,4,"Post 2", 5, false, false))
+    println(WallService.update(Post(2, 3, 4, "another post!", 12, false, false)))
     WallService.printPosts()
 }
