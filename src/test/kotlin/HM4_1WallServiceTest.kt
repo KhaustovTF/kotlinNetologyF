bfea183e099edf481c6e8f6223f75b0f253b56ae
Khaustov T.F. <trofimhaustov@yandex.ru>
2025-04-15 22:11:59 +0800

    whats wrong with addfun?

--- src/test/kotlin/HM4_1WallServiceTest.kt
+++ src/test/kotlin/HM4_1WallServiceTest.kt
@@ -6,26 +6,17 @@ import org.junit.Assert.*
   private var posts = emptyArray<Post>()
   private var unickId = 0
 
-//@Test
-// fun add() {
-// posts += (++unickId)
-// return posts.last()
-// val result = WallService.add(Post(1,2,3,"Post", 4, false, false))
-//
-// assertEquals(1, 2, 3, "Post", 4, false, false, result)
-// }
+@Test
+ fun add() {
+ val result = WallService.add(Post(1,2,3,"Post", 4, false, false))
+
+ assertEquals(1, 2, 3, "Post", 4, false, false, result)
+ }
 
 @Test
  fun updateRealId() {
   WallService.add(Post(1,3,4,"Post", 5,false,false))
   val result = WallService.update(Post(1, 3, 4, "another post!", 12, false, false))
- for ((index,post) in posts.withIndex()) {
-  if (post.id == post.id){
-   posts[index] = post.copy(likes = post.likes.copy())
-   println(true)
-  }
- }
- println(false)
 
  assertEquals(true, result)
 }
@@ -33,16 +24,9 @@ import org.junit.Assert.*
 
   @Test
   fun updateUnrealId() {
-   WallService.add(Post(1,3,4,"Post", 5,false,false))
+//   WallService.add(Post(1,3,4,"Post", 5,false,false))
    val result = WallService.update(Post(2,3,4,"Post", 5,false,false))
-   for ((index,post) in posts.withIndex()) {
-    if (post.id == post.id){
-     posts[index] = post.copy(likes = post.likes.copy())
-     println(true)
-    }
-   }
-   println(false)
 
-   assertEquals(false, result)
+   assertEquals(true, result)
   }
 }
