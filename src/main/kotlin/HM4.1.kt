7fefce056fa98bd3c20fad7948f92543204b8a35
Khaustov T.F. <trofimhaustov@yandex.ru>
2025-05-02 21:56:19 +0800

    nothing wrong with test

--- src/main/kotlin/HM4.1.kt
+++ src/main/kotlin/HM4.1.kt
@@ -1,5 +1,3 @@
-import java.util.Date
-
 data class Post(
     val id: Int,
     val fromId: Int,
@@ -12,6 +10,46 @@ data class Post(
 
     val likes: Likes = Likes(0, true)
 )
+
+data class Likes(val count: Int, val userLikes: Boolean)
+
+
+abstract class Attachment {
+    abstract val type: String
+
+
+}
+
+data class PhotoAttachment(val photo: Photo) : Attachment() {
+    override val type: String = "Photo"
+}
+
+data class Photo(val id: Int, val ownerId: Int, val photo130: String, val photo604: String)
+
+data class VideoAttachment(val video: Video) : Attachment() {
+    override val type: String = "Video"
+}
+
+data class Video(val id: Int, val ownerId: Int, val name: String, val duration: Int)
+
+data class StickerAttachment(val sticker: Sticker) : Attachment() {
+    override val type: String = "Sticker"
+}
+
+data class Sticker(val id: Int, val ownerId: Int, val stickerId: Int)
+
+data class AudioAttachment(val audio: Audio) : Attachment() {
+    override val type: String = "Audio"
+}
+
+data class Audio(val id: Int, val ownerId: Int, val artist: String, val date: Int)
+
+data class FileAttachment(val file: File) : Attachment() {
+    override val type: String = "File"
+}
+
+data class File(val id: Int, val ownerId: Int, val title: String, val size: Int, val ext: String)
+
 data class Comment(
     val id: Int,
     val fromId: Int,
@@ -19,18 +57,14 @@ data class Comment(
     val date: Int
 )
 
-data class Likes(val count: Int, val userLikes: Boolean)
-
-
-
-class PostNotFoundException(message:String): Exception(message)
+class PostNotFoundException(message: String) : Exception(message)
 
 object WallService {
     private var posts = emptyArray<Post>()
     private var comments = emptyArray<Comment>()
     private var unickId = 1
 
-    fun getUnickId () = unickId
+    fun getUnickId() = unickId
 
     fun clear() {
         posts = emptyArray()
@@ -46,16 +80,16 @@ object WallService {
     fun update(post2: Post): Boolean {
         for ((index, post) in posts.withIndex()) {
             if (post2.id == post.id) {
-                posts[index] = post2.copy(likes = post2.likes.copy())
+                posts[index] = post2
                 return true
             }
         }
         return false
     }
 
-    fun createComment(postId: Int, comment: Comment): Comment{
-        for (post in posts){
-            if (post.id == postId){
+    fun createComment(postId: Int, comment: Comment): Comment {
+        for (post in posts) {
+            if (post.id == postId) {
                 comments += comment
                 return comment
             }
@@ -81,7 +115,7 @@ fun main() {
             7,
             false,
             false,
-            FileAttachment(File(1, 1, "Troyan",77,".exe")),
+            FileAttachment(File(1, 1, "Troyan", 77, ".exe")),
             Likes(30, true)
         )
     )
@@ -95,7 +129,7 @@ fun main() {
             6,
             false,
             false,
-            AudioAttachment(Audio(1, 1, "Queen",77)),
+            AudioAttachment(Audio(1, 1, "Queen", 77)),
             Likes(30, true)
         )
     )
@@ -158,54 +192,13 @@ fun main() {
 
 
     try {
-        val comment = WallService.createComment(22, Comment(1,2,"perviy", 333))
+        val comment = WallService.createComment(22, Comment(1, 2, "perviy", 333))
         println(comment)
-    } catch (e: PostNotFoundException){
+    } catch (e: PostNotFoundException) {
         println(e.message)
     }
-
-
-
-}
-
-abstract class Attachment {
-    abstract val type: String
-
-
 }
 
 
-data class PhotoAttachment(val photo: Photo) : Attachment() {
-    override val type: String = "Photo"
 
-}
 
-data class Photo(val id: Int, val ownerId: Int, val photo130: String, val photo604: String)
-
-data class VideoAttachment(val video: Video) : Attachment() {
-    override val type: String = "Video"
-
-}
-
-data class Video(val id: Int, val ownerId: Int, val name: String, val duration: Int)
-
-data class StickerAttachment(val sticker: Sticker) : Attachment() {
-    override val type: String = "Sticker"
-
-}
-
-data class Sticker(val id: Int, val ownerId: Int, val stickerId: Int)
-
-data class AudioAttachment(val audio: Audio) : Attachment() {
-    override val type: String = "Audio"
-
-}
-
-data class Audio(val id: Int, val ownerId: Int, val artist: String, val date: Int)
-
-data class FileAttachment(val file: File) : Attachment() {
-    override val type: String = "File"
-
-}
-
-data class File(val id: Int, val ownerId: Int, val title: String, val size: Int, val ext: String)
